fbef09204948f6f1a4adcf66743de94abbda4043
Penny Orengo <pennyorengo3@gmail.com>
2022-09-10 09:17:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** 
+ *main - Entry Point
+ *
+ *Return: Always 0(success)
+ */
+
+int main(void)
+{
+  /** string initialization*/
+  char Mystring[] = "Programming is like building a multilingual puzzle\n";
+  int val = puts(Mystring);
+
+  puts(Mystring);
+
+  return 0;
+}
