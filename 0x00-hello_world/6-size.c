266ff72f098ba796a6e351e893f98677eb1d8e72
Penny Orengo <pennyorengo3@gmail.com>
2022-09-10 12:34:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,16 +17,23 @@ long long int longlongintType;
 /**
  *sizeof - Initial Description;
  *
- *bytes\n" - Number of bytes
+ *byte(s)\n" - Number of bytes
  *
  *sizeof(intType) - Evaluates the size of a variable;
  */
 
-printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a float: %zu bytes\n", sizeof(floatType));
-printf("Size of a char: %zu byte\n", sizeof(charType));
-printf("Size of a long long int: %zu bytes\n", sizeof(longlongintType));
-printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 return (0);
+/**
+ *sizeof - Initial Description;
+ *
+ *byte(s)\n" - Number of bytes
+ *
+ *sizeof(intType) - Evaluates the size of a variable;
+ */
 
 }
