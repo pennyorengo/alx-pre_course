61b79797e129b0706c812a3cd8b4f00108819375
Penny Orengo <pennyorengo3@gmail.com>
2022-09-10 10:25:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry Point;
+ *
+ *return: Always 0(success)
+ */
+
+int main(void)
+{
+  /**
+   *string initialization
+   */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+ 
+return (0);
+}
