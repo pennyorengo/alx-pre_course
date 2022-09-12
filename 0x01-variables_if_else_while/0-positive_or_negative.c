1a226eaea5f2232703fabb1c2a72e00212d98f00
Penny Orengo <pennyorengo3@gmail.com>
2022-09-12 12:15:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,12 +23,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if(n>0);
+{
 printf("is positive\n");
-if(n=0);
-printf("is zero\n");
-if(n<0);
-printf("is negative\n");
+}
 
+if(n==0);
+{
+printf("is zero\n");
+}
+
+if(n<0);
+{
+printf("is negative\n");
+}
 return (0);
   
 }
