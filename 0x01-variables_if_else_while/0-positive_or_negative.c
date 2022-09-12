d594b12660213d551e2109d725a9250c10cafc24
Penny Orengo <pennyorengo3@gmail.com>
2022-09-12 12:05:10 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always +n or -n (success)
+ *
+ *Description: Function main inputs and outputs
+ */
+
+int main(void)
+  
+{
+
+int n;
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+if(n>0);
+printf("is positive\n");
+if(n=0);
+printf("is zero\n");
+if(n<0);
+printf("is negative\n");
+
+return (0);
+  
+}
