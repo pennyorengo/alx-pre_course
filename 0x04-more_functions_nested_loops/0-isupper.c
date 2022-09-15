8b8add3886b14ecb39e971e458677095df6b0e1b
Penny Orengo <pennyorengo3@gmail.com>
2022-09-15 04:55:14 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isupper - function that checks for uppercase character
+ * @c: is the int that will be used for the argument of function
  * Return: 1 on success
  */
 
 int _isupper(int c)
 {
-	if (c = 'A'; c <= 'Z'; c++)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
