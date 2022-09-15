b434f2d105079ff530631c0ebb336a1106267a0d
Penny Orengo <pennyorengo3@gmail.com>
2022-09-15 04:48:05 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * Return: 1 on success
+ */
+
+int _isupper(int c)
+{
+	if (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
