45dd299d1f55f1806df3f90d1c74f6d39cd71fcc
Penny Orengo <pennyorengo3@gmail.com>
2022-09-15 06:03:49 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0 to 9
+ * void: return value
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
