8e6debe26506eb740121df607a1c9f60803a331c
Penny Orengo <pennyorengo3@gmail.com>
2022-09-19 16:34:41 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
-
- *_puts - prints a string
-
+ * _puts - prints a string
  * @str: string to print
-
  *
-
  * Description: prints a string
-
- * On success: returns no error
-
+ * Return: On success returns no error
  */
-
-
 
 void _puts(char *str)
 
