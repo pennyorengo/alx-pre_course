b23f1374e3d43e2a3d4970ac494952235a6fe4ae
Penny Orengo <pennyorengo3@gmail.com>
2022-09-19 15:48:48 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,18 +4,11 @@
 /**
  * reset_to_98 - updated the value it points to to 98
  * @n: integer used for argument in function
- * @*p: pointer
- *Return: Always 0
+ *Return: nothing
  */
 
 void reset_to_98(int *n)
 {
-
-	int n;
-	n = 98;
-	*p = &n;
-	
-	_putchar(n);
-
-	return (0);
+  
+	*n = 98;
 }
